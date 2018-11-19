64a69a34bb169167adfbee01097541af6f335409
Stanislav Nikolov <stanleyoffer@gmail.com>
2018-11-19 15:10:20 +0200

    logo

--- LiverpoolFanShopApp/LiverpoolFanShopApp/Controllers/ProductsController.cs
+++ LiverpoolFanShopApp/LiverpoolFanShopApp/Controllers/ProductsController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LiverpoolFanShopApp.Data;
+using LiverpoolFanShopApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LiverpoolFanShopApp.Controllers
 {
-    public class ProductsController:BaseController
+    public class ProductsController : BaseController
     {
         private readonly ApplicationDbContext db;
 
@@ -18,14 +19,34 @@ namespace LiverpoolFanShopApp.Controllers
         public IActionResult All()
         {
             var products = this.db.Products.ToList();
-            
+
             return View(products);
         }
         public IActionResult Index(int id = 23)
         {
-            var product = this.db.Products.FirstOrDefault(x => x.Id == id);
-
-            return View(product);
+            //var product = this.db.Products.FirstOrDefault(x => x.Id == id);
+            var pictures = new List<Picture>
+            {
+                new Picture
+                     {
+                         Id = 1,
+                         Name = "kit",
+                         PathUrl = "/images/Categories/kit/manShirt.jpg",
+                     }
+            };
+            var product2 = new Product
+            {
+                Id = 1,
+                Name = "Gosho",
+                Category = new Category
+                {
+                    Id = 1,
+                    Name = "fan"
+                },
+                Price = 15.99m,
+                Pictures = pictures,
+            };
+            return View(product2);
         }
     }
 }
